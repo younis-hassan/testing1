f3e44e7cc1e4338455b7c3613073317e16eb74f5
younis hassan <younishassan2007@yahoo.com>
2022-10-22 01:09:33 +0200

    second commit

--- src/test/java/open.java
+++ src/test/java/open.java
@@ -3,7 +3,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class open {
     public static void main(String[] args) throws InterruptedException {
-
+//define path
         String path="c://chromedriver//chromedriver.exe";
         WebDriver dd=new ChromeDriver();
         System.setProperty("webdriver.chrome.driver","path");
@@ -11,6 +11,7 @@ public class open {
         dd.navigate().to("http://www.google.com");
         Thread.sleep(5000);
         dd.close();
+
 
 
 
